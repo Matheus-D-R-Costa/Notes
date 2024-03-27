30e5f4593e6d8853d1dcd77c0c56dde509d2b755
Matheus Da Rosa Costa <161109851+Matheus-D-R-Costa@users.noreply.github.com>
2024-03-27 11:12:30 -0300

    add new functions

--- Design Patterns/factories/index.js
+++ Design Patterns/factories/index.js
@@ -2,3 +2,10 @@ import createCore from './core.js'
 
 const core = createCore()
 
+try {
+    core.start()
+    core.stop()
+} catch (error) {
+    console.log('[index] Uncaught error!')
+    console.log(error)
+}
